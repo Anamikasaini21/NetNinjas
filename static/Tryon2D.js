4fd8b2dfe45ebf9cfff04d68e246ebc79a89d8ab
dianasolomon <dianapsolomon@gmail.com>
2024-04-06 16:51:20 +0530

    2D tryon done

---
+++ static/Tryon2D.js
@@ -0,0 +1,72 @@
+
+function logSubmit(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);  
+
+  console.log(event.target)
+  const files = [...(data.entries())];
+  console.log("data",(data.entries()))
+  console.log(files)
+  const person = files[0][1];
+  const clothes = files[1][1];
+  console.log(person);
+  
+  const dataSend = new FormData();
+  dataSend.append("personImage", person);
+  dataSend.append("clothImage", clothes);
+  console.log("request");
+  const url = "https://virtual-try-on2.p.rapidapi.com/clothes-virtual-tryon";
+  const options = {
+    method: "POST",
+    headers: {
+     'X-RapidAPI-Key': 'e1644592aemshee05cc42095b149p18d88fjsn76ed55d7213d',
+		'X-RapidAPI-Host': 'virtual-try-on2.p.rapidapi.com'
+    },
+    body: dataSend,
+  };
+  try {
+    console.log("fetch call");
+    fetch(url, options)
+      .then((val) => {
+        // console.log(val);
+        return val.json();
+      })
+      .then((result) => {
+        console.log("inside")
+         console.log(result);
+         const key='response';
+         console.log(result[key]);
+         result = result[key];
+         const k='ouput_path_img';
+         const u=result[k];
+      
+         console.log(u);
+        //  const ur='"'+u+'"';
+        let d=document.querySelector("#res");
+        d.setAttribute("src",u);
+        var imageUrl =u; // Get the URL of the image from the link's href attribute
+
+        // var img = new Image();
+        // img.onload = function() {
+        //   document.getElementById("imageContainer").appendChild(img);
+        // };
+        // img.src = imageUrl;
+        // console.log(img.src)
+      
+      });
+      
+        console.log("url is printed");
+
+      console.log("call request end");
+  } catch (error) {
+    console.error(error);
+  }
+}
+const form = document.getElementById("images");
+form.addEventListener("submit", logSubmit);
+
+
+
+
+
+
